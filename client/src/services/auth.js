01ab0e3c0ea427010e510caced36f89fbdbfcdd9
Jose Augusto Gomes <jose.augusto.comparotto@gmail.com>
2021-02-17 23:49:28 -0400

    ok

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -2,7 +2,6 @@ import api from './api'
 
 export async function signIn({ login, password }) {
     try {
-
         const response = await api.post('/auth/authenticate', {}, {
             auth: {
                 username: login,
@@ -10,14 +9,15 @@ export async function signIn({ login, password }) {
             }
         })
 
-        const { status, data: { error = 'Erro inesperado' } } = response
+        const {status, data: { error = 'Erro inesperado' } } = response
 
         if (200 >= status && status <= 299) {
             return { status, ...response.data }
         }
 
-        return { status, error }
+        return { error }
+
     } catch (error) {
-        return { status: 400, error }
+        return { error: error.response.data.error }
     }
 }
